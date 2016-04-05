9c0543428d059f01a72309bd0a2625b36f696ba2
unknown <bkoenig@Webdev54.aclens.local>
2016-04-05 12:42:52 -0400

    STEP 5 CODE. Basic testing, first unit test complete, and basic xunit syntax

--- BadExample.Test/Services/EmailClientServiceTest.cs
+++ BadExample.Test/Services/EmailClientServiceTest.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BadExample.Service.Interfaces;
+using BadExample.Service.Services;
+using NSubstitute;
+using Xunit;
 
 namespace BadExample.Test.Services
 {
     public class EmailClientServiceTest
     {
+        private IEmailClientService _target;
+
+        [Fact]
+        public void LoginFails()
+        {
+            _target = new EmailClientService();
+
+            var result = _target.Login("BadUser", "BadPassword");
+
+            Assert.False(result);
+        }
     }
 }
